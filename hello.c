2d934ebd4533b55180c6ece53c7455acb5d31d47
Abraham X Hoskins <phoskins2@huskers.unl.edu>
2021-08-30 18:51:45 +0000

    added name and major

--- hello.c
+++ hello.c
@@ -3,8 +3,10 @@
 
   int main(int argc, char **argv) {
 
+  printf("Abraham X Hoskins!\n");
   printf("Braydon Weekly!\n");
 
+  printf("Major: Computer Enginnering!\n");
   printf("Major: Computer Enginnering!\n");
 
   return 0;
